4e9cba7c774302f9a66c85ca7f15a7b7e927306f
somal <somal1996@rambler.ru>
2017-01-01 17:45:54 +0300

    Add copying non-translated columns

--- multitran_scrapper/spiders/multitran.py
+++ multitran_scrapper/spiders/multitran.py
@@ -7,6 +7,7 @@ INPUT_CSV_NAME = 'input.csv'
 CSV_DELIMITER = '	'
 CSV_QUOTECHAR = '|'
 OUTPUT_CSV_NAME = 'output.csv'
+TRANSLATE_WORD_INDEX = 0
 
 
 class MultitranSpider(scrapy.Spider):
@@ -14,31 +15,35 @@ class MultitranSpider(scrapy.Spider):
     allowed_domains = ["multitran.ru"]
 
     def __init__(self):
-        input = open(INPUT_CSV_NAME, 'r')
-        self.input_reader = csv.reader(input, delimiter=CSV_DELIMITER, quotechar=CSV_QUOTECHAR,
+        input_file = open(INPUT_CSV_NAME, 'r')
+        self.input_reader = csv.reader(input_file, delimiter=CSV_DELIMITER, quotechar=CSV_QUOTECHAR,
                                        quoting=csv.QUOTE_ALL)
         self.output = []
 
     def start_requests(self):
         requests = []
-        for input_word in self.input_reader:
-            if len(input_word) > 0:
-                request = Request("http://www.multitran.ru/c/m.exe?s={}".format(input_word[0]), callback=self.translate,
-                                  meta={"input_word": input_word[0]})
+        for input_row in self.input_reader:
+            if len(input_row) > 0:
+                word = input_row[TRANSLATE_WORD_INDEX]
+                request = Request("http://www.multitran.ru/c/m.exe?s={}".format(word), callback=self.translate,
+                                  meta={"input_row": input_row})
 
                 requests.append(request)
         return requests
 
     def translate(self, response):
-        input_word = response.meta['input_word']
-        row_path = '//*/tr/td/table/tr/td/table/tr/td/table/tr/td/table/tr'
+        input_row = response.meta['input_row'][TRANSLATE_WORD_INDEX]
+        common_row_xpath = '//*/tr/td/table/tr/td/table/tr/td/table/tr/td/table/tr'
         translate_xpath = 'td[2]/a/text()'
         dict_xpath = 'td[1]/a/i/text()'
-        for row in response.xpath(row_path):
-            dictionary = row.xpath(dict_xpath).extract()
+        for common_row in response.xpath(common_row_xpath):
+            dictionary = common_row.xpath(dict_xpath).extract()
             if len(dictionary) > 0:
-                for translate in row.xpath(translate_xpath):
-                    self.output.append([input_word, dictionary[0], translate.extract()])
+                for translate in common_row.xpath(translate_xpath):
+                    output_array = response.meta['input_row'].copy()
+                    output_array.append(dictionary[0])
+                    output_array.append(translate.extract())
+                    self.output.append(output_array)
 
     def close(self, reason):
         output_file = open(OUTPUT_CSV_NAME, 'w')
