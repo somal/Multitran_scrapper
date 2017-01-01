553042e843832655a8a9e66a68d47b4649ef10f7
somal <somal1996@rambler.ru>
2017-01-01 16:46:14 +0300

    Power off robots.txt checng

--- multitran_scrapper/settings.py
+++ multitran_scrapper/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'multitran_scrapper.spiders'
 #USER_AGENT = 'multitran_scrapper (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
