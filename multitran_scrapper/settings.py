5f9bb17aab920fd6de8190a5e5b844e774c5b20a
somal <somal1996@rambler.ru>
2017-03-23 20:00:42 +0300

    Add download timeout

--- multitran_scrapper/settings.py
+++ multitran_scrapper/settings.py
@@ -24,6 +24,7 @@ ROBOTSTXT_OBEY = False
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 CONCURRENT_REQUESTS = 10
 LOG_LEVEL='INFO'
+DOWNLOAD_TIMEOUT = 240
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
