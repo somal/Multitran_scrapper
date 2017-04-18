1421a24ddea7add7019735483a0137a58fa74a9c
somal <somal1996@rambler.ru>
2017-04-18 19:01:36 +0300

    Fix settings

--- multitran_scrapper/settings.py
+++ multitran_scrapper/settings.py
@@ -66,9 +66,7 @@ DOWNLOAD_TIMEOUT = 240
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-ITEM_PIPELINES = {
-   'multitran_scrapper.pipelines.MultitranScrapperPipeline': 300,
-}
+ITEM_PIPELINES = {}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
